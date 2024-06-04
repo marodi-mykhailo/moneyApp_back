f0b19095b6d2e2d451b7b6cd5dbd0d956ad5740c
marodi-mykhailo <61832248+marodi-mykhailo@users.noreply.github.com>
2024-06-04 22:47:51 +0200

    MAPP-3: finish host db (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@app/user/entities/user.entity';
+import * as process from 'process';
+import { ConfigModule } from '@nestjs/config';
 
+
+//TODO: винести TypeOrmModule в окремий файл
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    username: process.env.DB_USER_NAME,
+    password: process.env.DB_PASS,
+    port: Number(process.env.DB_PORT),
+    database: process.env.DB_NAME,
+    synchronize: true,
+    logging: true,
+    entities: [User],
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+}
