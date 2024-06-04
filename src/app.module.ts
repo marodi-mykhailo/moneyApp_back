e9325a3d94f3ce8c287b206c0e37e7f7415449d5
mykhailomarodi <mykhailo.marodi@gmail.com>
2024-06-04 22:49:34 +0200

    MAPP-4: Create Resource (User)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,28 +1,30 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+import { AppController } from '@app/app.controller';
+import { AppService } from '@app/app.service';
+import { UserModule } from '@app/user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from '@app/user/entities/user.entity';
 import * as process from 'process';
 import { ConfigModule } from '@nestjs/config';
 
-
 //TODO: винести TypeOrmModule в окремий файл
 @Module({
-  imports: [ConfigModule.forRoot(), TypeOrmModule.forRoot({
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    username: process.env.DB_USER_NAME,
-    password: process.env.DB_PASS,
-    port: Number(process.env.DB_PORT),
-    database: process.env.DB_NAME,
-    synchronize: true,
-    logging: true,
-    entities: [User],
-  }), UserModule],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER_NAME,
+      password: process.env.DB_PASS,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      synchronize: true,
+      logging: true,
+      entities: [User],
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { UserService } from '@app/user/user.service';
+import { UserController } from '@app/user/user.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '@app/user/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
   providers: [UserService],
 })
